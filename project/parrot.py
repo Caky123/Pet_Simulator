7547fda9624413a42880ab358fa7ced8b9964ff0
Jan Cakl <cakl.jan@gmail.com>
2025-08-19 17:37:46 +0200

    Update parrot.py

--- project/parrot.py
+++ project/parrot.py
@@ -5,8 +5,10 @@ class Parrot(Pet):
     def __init__(self, name, hunger=50, energy=50, happiness=15, instincts=None):
         super().__init__(name, hunger, energy, happiness, instincts)
 
+    # Try generate random sound :)
     def make_sound(self):
         print("Hello!")
-
+    
+    # Check length of word, don't repeat a very long word.
     def repeat_word(self, word):
         print(word)
